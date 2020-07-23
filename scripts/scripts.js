363da429aab4cdbf1b0367cafb98b96d4d4fa589
ashwin <alexwin75@gmail.com>
2020-07-22 20:38:59 -0400

    getting header section and some styles ready ::rocket

---
+++ scripts/scripts.js
@@ -0,0 +1,53 @@
+// creating a namespace
+const PorschePicker = {};
+
+// build out a Porsche inventory 
+PorschePicker.inventory = {
+    coffee: [{
+            title: 'Early Bird',
+            price: '$$'
+        },
+        {
+            title: 'Dark Horse',
+            price: '$'
+        },
+        {
+            title: 'Le Gourmand',
+            price: '$'
+        }
+    ],
+    tea: [{
+            title: 'David\'s Tea',
+            price: '$$'
+        },
+        {
+            title: 'Tealish',
+            price: '$$'
+        },
+        {
+            title: 'Teavana',
+            price: '$'
+        }
+    ]
+};
+
+
+
+
+
+//build the randomiser
+
+//
+
+
+
+
+
+//getting things started
+$(document).ready()({
+
+
+
+
+
+});
